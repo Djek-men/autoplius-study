74a5eb45da74419951125e89d7c086f8c82b207b
Djek <Djek_nezlaya@mail.ru>
2015-09-24 08:34:01 +0200

    second commit

---
+++ local/resources/views/index.blade.php
@@ -0,0 +1,54 @@
+<?php
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset='utf-8'>
+        <title>Autoplius.by</title>
+		<meta name='description' content='auto'>
+		<meta name='keywords' content='auto'>
+		<link href='{{asset("media/bootstrap/css/bootstrap.min.css")}}' rel="stylesheet" type="text/css">
+        <link href='{{asset("media/css/style.css")}}' rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id='header'>
+		</div>
+		<nav class='menu-top'>
+		<a href='#'>Главная</a>
+		<a href='#'>Новости</a>
+		<a href='#'>Контакты</a>
+		</nav>
+
+		<div class='container'>
+		<div class='col-md-3'>меню
+		<a href='#'class="btn btn-success btn-block">Главная</a>
+		<a href='#'class="btn btn-success btn-block">Новости</a>
+		<a href='#'class="btn btn-success btn-block">Контакты</a>
+		</div>
+		<div class='col-md-6'> Добро пожаловать на сайт </div>
+		<div class='col-md-3'>
+
+				<table width="100%" height="180" style="background-color:#f2f2f2; border: #cccccc 1px solid; font-family:Tahoma; font-size:12px; color:#000000;" cellpadding="2" cellspacing="0">
+				<tr><td>
+				</td></tr>
+				<tr><td>
+
+				<table width=100% height=100% style="background-color:#f2f2f2; font-family:Tahoma; font-size:12px; color:#000000;" cellpadding="0" cellspacing="0">
+				<tr><td>
+				<script type="text/javascript" charset="windows-1251" src="http://pogoda.by/meteoinformer/js/26850_3.js"></script>
+				</td></tr>
+				</table>
+
+				</td></tr>
+
+				<tr><td align="right">
+				</td></tr>
+				</table>
+
+		</div>
+
+		<div style='clear:both'></div>
+        <div id='footer' class='container footer'>
+
+		</div>
+    </body>
+</html>
